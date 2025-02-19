a81482f103c277929802d47ecd9382a2813e47ce
Rostarc <147600695+Rostarc@users.noreply.github.com>
2025-02-19 19:46:05 +0200

    Create navigation.php

---
+++ inc/navigation.php
@@ -0,0 +1,11 @@
+<!-- /var/www/html/inc/navigation.php -->
+<nav class="main-nav">
+    <ul>
+        <li><a href="?page=openvpn">OpenVPN</a></li>
+        <li><a href="?page=wireguard">WireGuard</a></li>
+        <li><a href="?page=logs">Логи</a></li>
+        <li><a href="?page=update">Обновление</a></li>
+        <li><a href="?page=console">Консоль</a></li>
+        <li><a href="?page=monitoring">Мониторинг</a></li>
+    </ul>
+</nav>
