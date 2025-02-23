fd5f5c2d679dd1b80687163c7b72a83c05cc6e64
Rostarc <147600695+Rostarc@users.noreply.github.com>
2025-02-23 16:49:23 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,11 @@
 <!-- /var/www/html/inc/footer.php -->
     <footer>
-        <p>© <?php echo date('Y'); ?> VPN Management</p>
+        <span>© <?php echo date('Y'); ?> VPN Panel v<?php echo SITE_VERSION; ?></span>
+        &nbsp;|&nbsp;
+        <a href="https://t.me/vpn_vendor" target="_blank">
+            <!-- Если есть своя иконка телеграм, вставьте <img src="/img/telegram-icon.png" ...> -->
+            Поддержка в Telegram
+        </a>
     </footer>
 </body>
 </html>
