80e4c33d54ab29d802d7b634b5726f9ec63d7c43
Rostarc <147600695+Rostarc@users.noreply.github.com>
2025-02-23 16:50:30 +0200

    Update navigation.php

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,11 +1,11 @@
-<!-- /var/www/html/inc/navigation.php -->
 <nav class="main-nav">
     <ul>
-        <li><a href="?page=openvpn">OpenVPN</a></li>
-        <li><a href="?page=wireguard">WireGuard</a></li>
-        <li><a href="?page=logs">Логи</a></li>
-        <li><a href="?page=update">Обновление</a></li>
-        <li><a href="?page=console">Консоль</a></li>
-        <li><a href="?page=monitoring">Мониторинг</a></li>
+        <li><a href="/">Главная</a></li>
+        <li><a href="/openvpn">OpenVPN</a></li>
+        <li><a href="/wireguard">WireGuard</a></li>
+        <li><a href="/monitoring">Мониторинг</a></li>
+        <li><a href="/logs">Логи</a></li>
+        <li><a href="/update">Обновление</a></li>
+        <li><a href="/console">Консоль</a></li>
     </ul>
 </nav>
