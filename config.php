50abe97ef342faedd383ad0d1c80de6a74ae9689
Rostarc <147600695+Rostarc@users.noreply.github.com>
2025-02-24 22:55:00 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // /var/www/html/config.php
 
 // Версия сайта
-define('SITE_VERSION', '2.0');
+define('SITE_VERSION', '2.3');
 
 // Путь к лог-файлу веб-приложения
 define('WEB_APP_LOG', '/var/log/vpn-web.log');
