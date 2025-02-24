1c52896524e9c0676382be6f2e081629f91037a2
Rostarc <147600695+Rostarc@users.noreply.github.com>
2025-02-24 15:02:19 +0200

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # /var/www/html/scripts/update.sh
 
-cd /var/www/html
+# Перейти в каталог сайта
+cd /var/www/html || exit
+
+# Убедимся, что каталог отмечен как безопасный для Git (если требуется)
 sudo git config --global --add safe.directory /var/www/html
-sudo git pull origin main
+
+# Получить последние изменения из репозитория
+sudo git fetch origin main
+
+# Сбросить все локальные изменения и обновить рабочую копию до состояния ветки main
+sudo git reset --hard origin/main
 
 echo "Скрипт обновления завершён"
