c7cbd98d0e275a95b1e5fd48ee98d79271795281
Rostarc <147600695+Rostarc@users.noreply.github.com>
2025-02-28 16:10:46 +0200

    Add files via upload

--- elfinder/php/connector.minimal.php
+++ elfinder/php/connector.minimal.php
@@ -154,9 +154,6 @@ $opts = array(
 			'URL'           => dirname($_SERVER['PHP_SELF']) . '/home', // URL to files (REQUIRED)
 			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
-			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
-			'uploadAllow'   => array('image/x-ms-bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/x-icon', 'text/plain', 'text/php', 'text/sh'), // Mimetype `image` and `text/plain` allowed to upload
-			'uploadOrder'   => array('deny', 'allow'),      // allowed Mimetype `image` and `text/plain` only
 			'accessControl' => 'access'                     // disable and hide dot starting files (OPTIONAL)
 		),
 		// Trash volume
@@ -166,9 +163,6 @@ $opts = array(
 			'path'          => '/home/.trash',
 			'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '/home/.trash/.tmb/',
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
-			'uploadDeny'    => array('all'),                // Recomend the same settings as the original volume that uses the trash
-			'uploadAllow'   => array('image/x-ms-bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/x-icon', 'text/plain'), // Same as above
-			'uploadOrder'   => array('deny', 'allow'),      // Same as above
 			'accessControl' => 'access',                    // Same as above
 		),
 	)
